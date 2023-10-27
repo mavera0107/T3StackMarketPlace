dca018d947a67e8a9d6e90a61d308e4042d3398e
Muhammad Waqar <muhammadwaqar.business@gmail.com>
2023-10-27 11:11:20 +0500

    deployment issue fix

--- src/components/ui/ui/card.tsx
+++ src/components/ui/ui/card.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 
-import { cn } from "/src/components/libs/utils"
+import { cn } from "~/components/libs/utils";
 
 const Card = React.forwardRef<
   HTMLDivElement,
--- src/components/ui/ui/dropdown-menu.tsx
+++ src/components/ui/ui/dropdown-menu.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu"
 import { Check, ChevronRight, Circle } from "lucide-react"
 
-import { cn } from "/src/components/libs/utils"
+import { cn } from "~/components/libs/utils";
 
 const DropdownMenu = DropdownMenuPrimitive.Root
 
